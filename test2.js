7d9a03d616685e8895f6266e90bc5bff1a728e99
Alexander <thesolve@mail.ru>
2020-12-09 20:16:39 +0300

    Добавил переименование параметров функций

--- test2.js
+++ test2.js
@@ -3,7 +3,24 @@ layout: js_minifier
 ---
 
 'use strict';
-let days = '123';
-const sdf = 133;
-console.log(`${days } ${days + sdf }`);
-console.log('fef');
+const sdf = 5;
+console.log(asd(3, 4, sdf));
+
+function asd(ff, bg, ch)
+{
+	return ff * bg / ch;
+}
+
+function hlk(hj, er)
+{
+	return hj(er);
+}
+
+function wer(er)
+{
+	return hlk((uio) =>
+	{
+		return asd(3, 4, uio);
+	}, er);
+}
+console.log(wer(5));
