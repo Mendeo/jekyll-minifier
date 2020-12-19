07112b5f8ba7ce565824833dd539ad5ffe68bc21
Alexander <thesolve@mail.ru>
2020-12-20 00:24:19 +0300

    test file update

--- test2.js
+++ test2.js
@@ -4,11 +4,17 @@ layout: js_minifier
 
 'use strict';
 const sdf = 5;
-console.log(asd(3, 4, sdf));
+console.log(asd(3, sdf, 7));
+
+let jgf = 'function(as, df, gf)';
+if (jgf === 'function dwd (as, df, gh)')
+{
+	console.log('Ура!');
+}
 
 function asd(ff, bg, ch)
 {
-	return ff * bg / ch;
+	return ff * bg + ch;
 }
 
 function hlk(hj, er)
